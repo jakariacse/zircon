86a7a7fdeb4f192a86540c7e7cee0ffef3a56c8e
jishanjak <jakahossain@gmail.com>
2018-07-15 18:35:26 +0600

    nav and slider

--- template/js/main.js
+++ template/js/main.js
@@ -1,4 +1,25 @@
 (function ($) {
-    'use strict';
+	"use strict";
 
-})(jQuery);
+	jQuery(document).ready(function ($) {
+    
+		$('.homepage-slides').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,		    
+			navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+		})
+    
+  });
+}(jQuery));
